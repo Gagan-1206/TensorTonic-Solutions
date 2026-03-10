8189adb5de8747d5ee245a66b707633e70400000
Gagan-1206 <gagankamble1206@gmail.com>
2026-03-10 14:03:11 +0530

    Add solution for label-smoothing-loss

---
+++ label-smoothing-loss/label-smoothing-loss.py
@@ -0,0 +1,25 @@
+def label_smoothing_loss(predictions, target, epsilon):
+    """
+    Compute cross-entropy loss with label smoothing.
+    """
+    # Write code here
+import math
+
+def label_smoothing_loss(predictions, target, epsilon):
+    """
+    Compute cross-entropy loss with label smoothing.
+    """
+
+    K = len(predictions)
+
+    loss = 0.0
+
+    for i in range(K):
+        if i == target:
+            q = (1 - epsilon) + (epsilon / K)
+        else:
+            q = epsilon / K
+
+        loss += q * math.log(predictions[i])
+
+    return -loss
